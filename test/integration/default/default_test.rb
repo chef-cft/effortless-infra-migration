ebc982d4586198c662caa554ef40cbd048eec23a
Nathan Cerny <ncerny@gmail.com>
2019-04-03 12:27:28 -0500

    Update kitchen tests and usage.
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,14 +5,13 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe package('chef') do
+  it { should_not be_installed }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+# TODO: Update this to be Windows-Friendly
+describe command('/usr/bin/hab pkg list --all') do
+  it { should exist }
+  its('stdout') { should match /core\/hab/ }
+  its('stdout') { should match /chef\/chef-client/ }
 end
