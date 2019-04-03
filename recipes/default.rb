e600e76b12c6cb1cd7b034fa42799591e4d34afb
Nathan Cerny <ncerny@gmail.com>
2019-04-03 16:43:26 -0500

    Install habitat, binlink, and remove omnibus install.  Test it's all working. (#2)
    * move fixtures under test folder
    
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>
    
    * Update kitchen tests and usage.
    
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>
    
    * Install Habitat and Chef-Client
    
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>
    
    * Cleanup old omnibus install of chef-client and run chef-client again to ensure the hab-managed version is being installed.
    
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+hab_install
+
+hab_package 'chef/chef-client'
+
+package 'chef' do
+  action :remove
+end
+
+execute '/bin/hab pkg binlink chef/chef-client' do
+  not_if { ::File.exist?('/bin/chef-client') }
+end
--- test/fixtures/cookbooks/test/metadata.rb
+++ test/fixtures/cookbooks/test/metadata.rb
@@ -1,9 +1,9 @@
-name 'chef-client-hab-distro'
+name 'test'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
-description 'Installs/Configures chef-client-hab-distro'
-long_description 'Installs/Configures chef-client-hab-distro'
+description 'Installs/Configures test'
+long_description 'Installs/Configures test'
 version '0.1.0'
 chef_version '>= 13.0'
 
@@ -11,12 +11,10 @@ chef_version '>= 13.0'
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/chef-client-hab-distro/issues'
+# issues_url 'https://github.com/<insert_org_here>/test/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/chef-client-hab-distro'
-
-depends 'habitat'
+# source_url 'https://github.com/<insert_org_here>/test'
