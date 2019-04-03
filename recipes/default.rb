27c4bdf1369fedc3a58b88351704b1380b534b42
Nathan Cerny <ncerny@gmail.com>
2019-04-03 13:08:51 -0500

    Cleanup old omnibus install of chef-client and run chef-client again to ensure the hab-managed version is being installed.
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,13 @@
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
 hab_install
-hab_package 'chef/chef-client' do
-  binlink :force
+
+hab_package 'chef/chef-client'
+
+package 'chef' do
+  action :remove
+end
+
+execute '/bin/hab pkg binlink chef/chef-client' do
+  not_if { ::File.exist?('/bin/chef-client') }
 end
